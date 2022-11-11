b251894f0f73f680d4998c1b1a062018be08be23
VishnuKeshaveni100 <109942273+VishnuKeshaveni100@users.noreply.github.com>
2022-11-11 20:08:51 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,97 +1,24 @@
-resource "aws_instance" "ec2s3" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    count = 1
-    tags {
-      Name = "ec2s3"
-    }
+# Kubernetes provider
+# https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
+# The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
+# You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
-resource "aws_security_group" "onesec" {
-    name        = "onesec"
-    description = "to allow the inbound traffic"
-    vpc_id = var.vpc_id
-
-  ingress {
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks       = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "onesec"
-  }
-} 
-
-# Create an IAM role for the ec2 instance.
-resource "aws_iam_role" "ec2_s3_iam_role" {
-    name = "ec2_s3_iam_role"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-EOF
+provider "aws" {
+  region = var.region
 }
 
-resource "aws_iam_role_policy" "web_iam_role_policy" {
-  name = "web_iam_role_policy"
-  role = aws_iam_role.web_iam_role.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:*",
-                "s3-object-lambda:*"
-            ],
-            "Resource": "*"
-        }
-    ]
-EOF
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "education-eks-${random_string.suffix.result}"
 }
 
-resource "iam_role_policy_attachment" "role-policy-attachment" {
-    name = "role-policy"
-    roles      = aws_iam_role.ec2_s3_iam_role.name
-}
-
-resource "aws_iam_instance_profile" "test_profile" {
-  name  = "test"
-  roles = aws_iam_role.ec2_s3_iam_role.name
-}
-
-resource "aws_s3_bucket" "test_bucket" {
-    bucket = "one-bucket"
-    versioning {
-            enabled = false
-    }
-    tags {
-        Name = "one-bucket"
-    }
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
